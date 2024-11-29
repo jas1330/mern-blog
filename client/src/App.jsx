8d70e44820d362c2e2c671a3cb918006b3423b6f
jas1330 <97436838+jas1330@users.noreply.github.com>
2024-11-30 00:00:49 +0530

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,17 @@ import Dashboard from "./pages/Dashboard";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Projects from "./pages/Projects";
+import Header from "./components/Header";
 export default function App(){
   return(
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/about" element={<About />}/>
-      <Route path="/Signin" element={<Signin />}/>
-      <Route path="/Signup" element={<Signup />}/>
-      <Route path="/Dashboard" element={<Dashboard />}/>
+      <Route path="/signin" element={<Signin />}/>
+      <Route path="/signup" element={<Signup />}/>
+      <Route path="/dashboard" element={<Dashboard />}/>
       <Route path="/projects" element={<Projects />}/>
 
 
