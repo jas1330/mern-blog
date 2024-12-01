0a132f8c511d8886636594288cc2d012e6e65d1b
jas1330 <97436838+jas1330@users.noreply.github.com>
2024-12-01 10:20:54 +0530

    add redux persist

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -48,10 +48,10 @@ export default function SignIn() {
         {/* left */}
         <div className='flex-1'>
           <Link to='/' className='font-bold dark:text-white text-4xl'>
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-              Sahand's
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-blue-500 rounded-lg text-white'>
+              Blog
             </span>
-            Blog
+            Forge
           </Link>
           <p className='text-sm mt-5'>
             This is a demo project. You can sign in with your email and password
@@ -81,7 +81,7 @@ export default function SignIn() {
               />
             </div>
             <Button
-              gradientDuoTone='purpleToPink'
+              gradientDuoTone='purpleToBlue'
               type='submit'
               disabled={loading}
             >
